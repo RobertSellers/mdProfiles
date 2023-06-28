b22d0dc6335f55f737113e580a895f34f58e53e2
Robert Sellers <robert.sellers@usda.gov>
2023-06-28 15:39:38 -0400

    remove profiles for testing

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@
  */
 
 import full from '../resources/profiles/full.json';
-import basic from '../resources/profiles/basic.json';
 import raster from '../resources/profiles/raster.json';
-import product from '../resources/profiles/product.json';
 
 import schema from '../resources/profile-schema.json';
 import pkg from '../package.json';
@@ -13,7 +11,7 @@ import pkg from '../package.json';
 const version = pkg.version;
 
 function asArray() {
-  return [full, basic, raster, product];
+  return [full, raster];
 }
 
-export default { full, basic, raster, product, schema, asArray, version };
+export default { full, raster, schema, asArray, version };
